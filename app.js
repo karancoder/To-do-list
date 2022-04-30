e9350ff6a63657ef482b6c21971ec60a9c22e1bb
karancoder <jivanikaran@gmail.com>
2022-04-30 19:39:14 +0530

    till select added (just structure + css)

--- app.js
+++ app.js
@@ -7,24 +7,70 @@ const todoList = document.querySelector(".todo-list");
 
 // Event bindings
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteOrCheckTodo);
 
 // Event handlers
 
 function addTodo(event) {
     event.preventDefault();
-    console.log("hello");
-    createTodo();
+    addTodoDiv();
+}
+
+function deleteOrCheckTodo(event) {
+    const item = event.target;
+    if (item.classList[0] === "trash-btn") {
+        deleteTodoUsingBtn(item);
+    }
+    if (item.classList[0] === "check-btn") {
+        childTodoUsingBtn(item);
+    }
 }
 
 // Helper functions
 
-function createTodo() {
+function addTodoDiv() {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-    const newTodo = document.createElement("li");
-    newTodo.innerText = "Hello";
-    newTodo.classList.add("todo-item");
+    const newTodo = createTodo();
     todoDiv.appendChild(newTodo);
-
+    const checkTodoButton = createCheckTodoButton();
+    todoDiv.appendChild(checkTodoButton);
+    const trashTodoButton = createTrashTodoButton();
+    todoDiv.appendChild(trashTodoButton);
     todoList.appendChild(todoDiv);
+}
+
+function createTodo() {
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoInput.value = "";
+    return newTodo;
+}
+
+function createCheckTodoButton() {
+    const checkTodoButton = document.createElement("button");
+    checkTodoButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkTodoButton.classList.add("check-btn");
+    return checkTodoButton;
+}
+
+function createTrashTodoButton() {
+    const trashTodoButton = document.createElement("button");
+    trashTodoButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashTodoButton.classList.add("trash-btn");
+    return trashTodoButton;
+}
+
+function deleteTodoUsingBtn(item) {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", () => {
+        todo.remove();
+    });
+}
+
+function childTodoUsingBtn(item) {
+    const todo = item.parentElement;
+    todo.classList.toggle("checked");
 }
